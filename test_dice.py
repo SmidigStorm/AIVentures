785303a8e2d50b23f42b1b9dd8a9f3117d782231
thommyb <thommy@gnist.as>
2023-02-04 09:49:01 +0100

    Initial commit

---
+++ test_dice.py
@@ -0,0 +1,46 @@
+import unittest
+from dice import Dice
+
+class TestDice(unittest.TestCase):
+    def test_roll_d4(self):
+        # Arrange
+        dice = Dice()
+
+        # Act
+        result = dice.roll_d4()
+
+        # Assert
+        self.assertTrue(1 <= result <= 4)
+
+    def test_roll_d6(self):
+        # Arrange
+        dice = Dice()
+
+        # Act
+        result = dice.roll_d6()
+
+        # Assert
+        self.assertTrue(1 <= result <= 6)
+
+    def test_roll_d8(self):
+        # Arrange
+        dice = Dice()
+
+        # Act
+        result = dice.roll_d8()
+
+        # Assert
+        self.assertTrue(1 <= result <= 8)
+
+    def test_roll_d20(self):
+        # Arrange
+        dice = Dice()
+
+        # Act
+        result = dice.roll_d20()
+
+        # Assert
+        self.assertTrue(1 <= result <= 20)
+
+if __name__ == '__main__':
+    unittest.main()
