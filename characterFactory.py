4db6de1436166f592c7c8b5ba18d35c0d1ea85d8
thommyb <thommy@gnist.as>
2023-02-04 11:12:54 +0100

    added characterfactory to set default values from json file

--- characterFactory.py
+++ characterFactory.py
@@ -7,15 +7,22 @@ class CharacterFactory:
         with open("races.json") as f:
             self.races = json.load(f)
 
+        with open("races_default_values.json") as default_values_file:
+            self.races_defaults = json.load(default_values_file)
+
     def create_character(self, name, race, class_name, strength, dexterity, constitution, intelligence, wisdom,
                          charisma):
         race_stats = self.races[race]
+        race_default_values = self.races_defaults[race]
+
         char = Character()
 
-        character = char.Character(name, race, class_name, strength + race_stats["strength_bonus"], dexterity + race_stats["dexterity_bonus"],
-                              constitution + race_stats["constitution_bonus"],
-                              intelligence + race_stats["intelligence_bonus"],
-                              wisdom + race_stats["wisdom_bonus"], charisma + race_stats["charisma_bonus"])
+        character = char.Character(name, race, class_name, race_default_values["strength"] + race_stats["strength_bonus"],
+                              race_default_values["dexterity"] + race_stats["dexterity_bonus"],
+                              race_default_values["constitution"] + race_stats["constitution_bonus"],
+                              race_default_values["intelligence"] + race_stats["intelligence_bonus"],
+                              race_default_values["wisdom"] + race_stats["wisdom_bonus"],
+                              race_default_values["charisma"] + race_stats["charisma_bonus"])
         return character
 
 # 1d6: 4 rolls remove lowest, choose BEST one. Instead of default
