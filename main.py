277916f3edb8fd1adcbd3b2e51325f5dd73d2584
thommyb <thommy@gnist.as>
2023-03-18 10:27:25 +0100

    comments

--- main.py
+++ main.py
@@ -30,13 +30,12 @@ print(monster.get_stats())
 # BATTLE #
 battle = Battle(character, monster)
 
-
 initiative_list = battle.calculate_initiative() # Who goes first
 for item in initiative_list:
     print(f"{item[0]} has an initiative roll of {item[1]}")
 
-winner = battle.run_battle()
-battle.end_battle(winner == "player")
+winner = battle.run_battle() # FIGHT
+battle.end_battle(winner == "player") # Give out xp and levels to player if they won
 print("---- AFTER BATTLE ----")
 print(character.get_stats())
 print(monster.get_stats())
