d0827965b623f90233b549eb5c0d1259d1c00833
thommyb <thommy@gnist.as>
2025-02-11 13:51:46 +0100

    More prep for armors so tbe added to make real ac a thing

--- armor.py
+++ armor.py
@@ -1,11 +1,8 @@
 from equipment import Equipment
 from equipmentType import EquipmentType
 
-
 class Armor(Equipment):
-    def __init__(self, name: str, ac_bonus: int, equipment_type: EquipmentType):
-        super().__init__(name, equipment_type)
-        self.ac_bonus = ac_bonus
-
-    def get_ac_bonus(self):
-        return self.ac_bonus
+    def __init__(self, name: str, base_ac: int, category: str):
+        super().__init__(name, EquipmentType.ARMOR, {})
+        self.base_ac = base_ac # Light and Medium should be added to with dex bonus..
+        self.category = category  # "Light", "Medium", or "Heavy"
